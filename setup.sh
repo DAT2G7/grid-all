60be40b468f90fc7ad70e80f3a4bc0aca4cfae8e
bliztle <asbjoern.r.e@gmail.com>
2023-04-03 13:10:10 +0200

    Jest types. Tricks intellisense with submodules

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 # No shebang for attempted system independence
-echo "Setting up all sup-repositories of the grid"
+echo "Setting up all sub-repositories of the grid"
 
 # install node_modules
 echo "Installing node_modules"
+npm install
 npm --prefix ./grid-core install ./grid-core
 npm --prefix ./grid-project install ./grid-project
 npm --prefix ./grid-server install ./grid-server
